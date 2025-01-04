43b7d71f30e663ce4ecaad8c04e0bbe9e379a0f8
Rithvik <nrithvik19@gmail.com>
2025-01-04 19:35:37 +0530

    Final Changes..

--- Client/src/store/Auth-Slice.ts
+++ Client/src/store/Auth-Slice.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create axios instance with proper configs
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.MODE==='development'? 'http://localhost:3000/api':'/api',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json'
--- Client/src/store/Todo-Slice.ts
+++ Client/src/store/Todo-Slice.ts
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.MODE==='development'? 'http://localhost:3000/api':'/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
