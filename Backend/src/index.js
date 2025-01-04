612897a47b43800d0fb7b86e8639dc68a989404b
Rithvik <nrithvik19@gmail.com>
2025-01-04 19:14:37 +0530

    Deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -27,13 +27,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 
-/*if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Client", "dist", "index.html"));
   });
-}*/
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
